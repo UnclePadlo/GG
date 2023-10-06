8a9a36d5021051e994abb14f1697907b325b5c9c
Svyatoslav Galah <svyatik324@gmail.com>
2023-10-06 18:20:40 +0300

    First explain

---
+++ Lesson_5/Lesson_5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int gg = 10;
+
+	for (int i = 0; i < gg; i++)
+	{
+		for (int j = 0; j < gg; j++)
+		{
+			// область під головною та 
+			// під побічною діагоналями
+			if (i + j >= 9 && i >= j)
+				cout << "|===|";
+			else
+				cout << "     ";
+		}
+		cout << endl;
+	}
+	/*cout <<"|                                                |" << endl;*/
+	cout << "|\t\t\t\t\t\t |" << endl;
+
+	for (int a = 0; a < gg; a++)
+	{
+		for (int i = 0; i < gg; i++)
+		{
+			cout << "|###|";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
